8c73e31bbd6a53255c33e0e5819ad958b3e2f554
Shruti Aote <shrutiaote@gmail.com>
2026-09-16 18:03:55 +0530

    Initial MyCaretaker backend setup

---
+++ src/main/java/com/mycaretaker/MycaretakerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mycaretaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycaretakerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycaretakerBackendApplication.class, args);
+	}
+
+}
